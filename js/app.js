5428b5017d56ec8ab9ff4e74ea6e519c1413eb48
Sahil Satpute <explore.sahil@outlook.com>
2022-10-27 16:20:24 +0530

    Updated

--- js/app.js
+++ js/app.js
@@ -8,12 +8,6 @@ listItems.forEach((li, i) => {
   li.addEventListener("mouseleave", () => {
     photos[i].classList.remove("intro");
   });
-  /*
-  li.addEventListener("mousemove", (e) => {
-    photos[i].style.left = -e.clientX * 0.5 + 80 + "px";
-    photos[i].style.top = -e.clientY * 0.5 + 100 + "px";
-  });
-  */
 });
 
 const listItemsa = document.querySelectorAll("li");
@@ -27,16 +21,3 @@ listItemsa.forEach((li, i) => {
     colors[i].classList.remove("colorChange");
   });
 });
-
-/*const x = document.querySelectorAll("li");
-bgcolor = document.getElementsByClassName("bgcolor");
-
-x.forEach((li, i) => {
-  li.addEventListener("mouseenter", () => {
-    bgcolor[i].classList.add("bgcolorselector");
-  });
-  li.addEventListener("mouseleave", () => {
-    bgcolor[i].classList.remove("bgcolorselector");
-  });
-});
-*/
